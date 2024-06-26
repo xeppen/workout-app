02116b2e2117d411bd0e5b207054782d6f641ed9
Sebastian Ljungberg <xeppen@gmail.com>
2024-06-26 17:07:12 +0200

    feat(workout-plans): add IsNumber validation to sets, reps, and restTime fields in ExerciseInPlanDto feat(workout-plans): create tests for ExerciseInPlanDto validation scenarios feat(workout-plans): add UpdateWorkoutPlanDto with optional fields validation build(api): update tsconfig.json with experimentalDecorators and emitDecoratorMetadata build(api): update reflect-metadata version to ^0.1.14 in package.json

--- apps/api/src/app/workout-plans/dto/execise-in-plan.dto.ts
+++ apps/api/src/app/workout-plans/dto/execise-in-plan.dto.ts
@@ -1,16 +1,19 @@
-import { IsString, IsInt, Min, IsNotEmpty } from 'class-validator';
+import { IsString, IsInt, Min, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class ExerciseInPlanDto {
   @IsString()
   @IsNotEmpty()
   exerciseId: string;
 
+  @IsNumber()
   @IsNotEmpty()
   sets: number;
 
+  @IsNumber()
   @IsNotEmpty()
   reps: number;
 
+  @IsNumber()
   @IsNotEmpty()
   restTime: number;
 }
